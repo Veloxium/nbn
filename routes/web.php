a6645b8430daae974d6107e3dfd080be15f9eea0
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-05 12:31:14 +0800

    fix : withspace

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 // routes/web.php
-
-use App\Http\Controllers\AdminController;
-use App\Http\Middleware\AdminMiddleware;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CheckoutController;
-use App\Models\Product;
-
 Route::get('/', fn() => view('welcome'));
 
 // Import semua modular route
