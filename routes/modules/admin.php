4c1f59573ebb4a3571dd64d58576d16d9019da75
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-07 02:01:08 +0800

    ref : move product route

--- routes/modules/admin.php
+++ routes/modules/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Middleware\AdminMiddleware;
+use App\Models\Product;
 
 Route::middleware(['auth', AdminMiddleware::class])->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
