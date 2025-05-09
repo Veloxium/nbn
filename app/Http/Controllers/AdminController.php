5198b27bc36afdb96f6d203ef49993a36a6cc639
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-09 14:49:58 +0800

    ref : dashboard and homepage

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\User;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
 
 class AdminController extends Controller
 {
-    public function dashboard()
+    public function dashboard() : View
     {
         // Tambahkan log untuk memastikan controller dipanggil
         Log::info('Admin dashboard accessed.');
         $userCount = User::count();
         $productCount = Product::count();
-        return view('admin.dashboard', compact('userCount', 'productCount'));  // Pastikan view ada dan benar
+        return view('admin.dashboard', compact('userCount', 'productCount'));
     }
 
 
