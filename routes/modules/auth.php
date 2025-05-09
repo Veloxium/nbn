41db5ab5da4afae3e6b2ddb292fcef2fb481ee19
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-09 14:54:00 +0800

    ref : remove unused route

--- routes/modules/auth.php
+++ routes/modules/auth.php
@@ -59,8 +59,3 @@ Route::middleware('auth')->group(function () {
 });
 
 
-// Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-// Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-// Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-// Route::post('/register', [RegisteredUserController::class, 'store']);
-// Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
