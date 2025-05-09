825160195481835841b8ff769af946424522c466
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-09 14:54:52 +0800

    fix : homepage route

--- routes/modules/user.php
+++ routes/modules/user.php
@@ -2,16 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Models\Product;
+use App\Http\Controllers\UserController;
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', fn() => view('user.profile'))->name('profile');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/home', function () {
-        $newProducts = Product::latest()->take(4)->get();
-        $allProducts = Product::latest()->get();
-        return view('user.home', compact('newProducts', 'allProducts'));
-    })->name('home');
+    Route::get('/homepage', [UserController::class, 'homepage'])->name('user.homepage');
 });
