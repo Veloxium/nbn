25a385c89d145a5d44f1b697d9770a5d2ac101c1
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-19 00:42:07 +0800

    feat : cod route

--- routes/modules/payment.php
+++ routes/modules/payment.php
@@ -8,6 +8,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/payments', [PaymentController::class, 'index'])->name('payments.index');
     Route::get('/payments/userform', [PaymentController::class, 'userForm'])->name('payments.userform');
     Route::get('/payments/credit/{id}', [PaymentController::class, 'credit'])->name('payments.credit');
+    Route::get('/payments/cod/{id}', [PaymentController::class, 'cod'])->name('payments.cod');
     Route::get('/payments/{payment}/proof', [PaymentController::class, 'showProofForm'])->name('payments.proof');
 
     Route::post('/payments/userform', [PaymentController::class, 'store'])->name('payments.userformstore');
@@ -17,6 +18,6 @@ Route::middleware(['auth'])->group(function () {
 
 
 Route::middleware(['auth', AdminMiddleware::class])->prefix('admin')->group(function () {
-    Route::resource('payments', PaymentController::class)->names('admin.payments');
+    Route::resource('/payments', PaymentController::class)->names('admin.payments');
 
 });
